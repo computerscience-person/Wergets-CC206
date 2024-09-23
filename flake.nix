158a304cd29ee915bbdccece762a877fd1516069
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-09-23 16:55:53 +0800

    Initial project repository setup.
    - Added flutter tooling.
    - Added android tooling.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = import nixpkgs { 
+              system = "x86_64-linux";
+              config.allowUnfree = true; 
+            };
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  android.enable = true;
+                  android.flutter.enable = true;
+
+                  enterShell = ''
+                    Environment with Android Tools and Flutter.
+                  '';
+
+                }
+              ];
+            };
+          });
+    };
+}
